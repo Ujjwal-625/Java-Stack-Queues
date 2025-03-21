9407ec90e32f2fdc266b677e61c8f66f59171b8e
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-21 21:01:54 +0530

    [Author:Ujjwal] ADD:SlidingWindowMaximum.java

---
+++ src/SlidingWindowMaximum.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class SlidingWindowMaximum {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || k <= 0) return new int[0];
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>(); // Stores indices of useful elements
+
+        for (int i = 0; i < n; i++) {
+            // Remove elements out of this window
+            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+            // Remove smaller elements in k range as they are useless
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            // Add current index
+            deque.offerLast(i);
+
+            // Store maximum for the window
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int[] result = maxSlidingWindow(nums, k);
+
+        System.out.println("Sliding Window Maximum: " + Arrays.toString(result));
+    }
+}
