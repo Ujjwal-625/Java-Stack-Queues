554e13301d070278dc6a698355727f446bf92d97
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-23 14:23:30 +0530

    [Author:Ujjwal ADD:LongestSubsequence.java

---
+++ src/Hashing/LongestSubsequence.java
@@ -0,0 +1,38 @@
+package Hashing;
+
+import java.util.*;
+
+public class LongestSubsequence {
+    public static int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) { // Check if it's the start of a sequence
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest Consecutive Sequence Length: " + longestConsecutive(nums));
+    }
+}
+
