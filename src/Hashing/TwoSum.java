58e2e8245f56bfdd3306da63dfa663677740f582
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-23 14:27:53 +0530

    [Author:Ujjwal ADD:TwoSum.java

---
+++ src/Hashing/TwoSum.java
@@ -0,0 +1,31 @@
+package Hashing;
+
+import java.util.*;
+
+public class TwoSum {
+    public static int[] findTwoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // Store number and its index
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{}; // No valid pair found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = findTwoSum(nums, target);
+
+        if (result.length > 0) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No pair found.");
+        }
+    }
+}
